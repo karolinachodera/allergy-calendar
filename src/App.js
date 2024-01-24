f04f2234718c91d8da7c30d7306ede8c90c8c72d
Karolina Chodera <karolinachodera@gmail.com>
2024-01-24 14:14:01 +0100

    start using Tomorrow API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const options = { method: "GET", headers: { accept: "application/json" } };
+
+  fetch(
+    "https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=XXX",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response))
+    .catch((err) => console.error(err));
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Allergy Calendar</p>
         <a
           className="App-link"
           href="https://reactjs.org"
